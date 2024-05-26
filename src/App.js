f5c456de6475e6bdd881e9315f1e2644069c6b01
chubebanso <vuquanghoanglam@gmail.com>
2024-05-27 02:38:08 +0700

    update

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import MyComponent from './component/MyComponent'
+const App = () => {
+
+
+  return (
+
+    <div>
+      <MyComponent>
+
+      </MyComponent>
+    </div>
+  );
+}
+
+export default App;
