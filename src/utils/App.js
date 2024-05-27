b97ac438e1a73391d8f0fd8e4efb466ba515beb2
chubebanso <vuquanghoanglam@gmail.com>
2024-05-28 00:51:22 +0700

    home page

---
+++ src/utils/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MyComponent from "../Component/Home/MyComponent";
+const App = () => {
+  return (
+    <div>
+      <MyComponent></MyComponent>
+    </div>
+  );
+};
+
+export default App;
