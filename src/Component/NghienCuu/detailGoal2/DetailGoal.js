6be7ae2642d9e8c1fba7635ad86f0fdcedc4a498
Taduc2003 <buitaduc139@gmail.com>
2024-05-30 22:50:51 +0700

    add-service

--- src/Component/NghienCuu/detailGoal2/DetailGoal.js
+++ src/Component/NghienCuu/detailGoal2/DetailGoal.js
@@ -1,41 +1,42 @@
 import React from "react";
 import "./DetailGoal.css";
-const DetailGoal = () => {
+const DetailGoal = (props) => {
+    
   return (
     <div className = "top-contents">
-        <div class="title">Các bài báo nghiên cứu trong kỳ</div>
+        <div class="title">Các {props.data.typee} trong kỳ</div>
         <div class="noidung">
             <div class="xemtatca">
                 <div class="bills">
                     <div class="detailss">
                         <div class="dates">
-                            <div class="semester">2023.1</div>
+                            <div class="semester">{props.data.semester}</div>
                         </div>
                         <div class="subscription-names">
                             <div class="texts">
-                                <div class="tenbaibao">Cải thiện độ tan và độ hòa tan của meloxicam với hệ phân tán rắn</div>
+                                <div class="tenbaibao">{props.data.name1}</div>
                                 <div class="ketthuc">Kết thúc: 08/07/2024</div>
                             </div>
                         </div>
                     </div>
                         <div class="amounts">
-                            <div class="trangthai">Đã công bố</div>
+                            <div class="trangthai">{props.data.status1}</div>
                         </div>
                 </div>
                 <div class="bills">
                     <div class="detailss">
                         <div class="dates">
-                            <div class="semester">2023.1</div>
+                            <div class="semester">{props.data.semester}</div>
                         </div>
                         <div class="subscription-names">
                             <div class="texts">
-                                <div class="tenbaibao">Cải thiện độ tan và độ hòa tan của meloxicam với hệ phân tán rắn</div>
+                            <div class="tenbaibao">{props.data.name2}</div>
                                 <div class="ketthucs">Kết thúc: 09/07/2024</div>
                             </div>
                         </div>
                     </div>
                     <div class="amounts">
-                        <div class="trangthai">Chưa công bố</div>
+                        <div class="trangthai">{props.data.status2}</div>
                     </div>
                 </div>
             </div>
