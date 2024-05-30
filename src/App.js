c29ad9787e8db6c02e96459193d8e16e2c8d4ebc
hoangQuan25 <quan.nh215127@sis.hust.edu.vn>
2024-05-30 11:09:33 +0700

    modify overview page

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MyComponent from "./Component/Home/MyComponent";
 import Login from "./Component/Auth/Login";
 import Register from "./Component/Auth/Register";
-<<<<<<< Updated upstream
 import Overview from "./Component/Overview/Overview";
 import Teach from "./Component/Teach/Teach";
 import Schedule from "./Component/Overview/Schedule/Schedule";
+import Header from "./Component/header/Header";
+import NotificationBar from "./Component/Overview/notificationBar/NotificationBar";
+import Sidebar from "./Component/Sidebar";
 
 const App = () => {
   return (
@@ -15,26 +17,11 @@ const App = () => {
         <Route path="/" element={<MyComponent />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/schedule" element={<Schedule />} />
+        {/* <Route path="/schedule" element={<Schedule />} /> */}
         <Route path="/overview" element={<Overview />} />
         <Route path="/teach" element={<Teach />} />
       </Routes>
     </Router>
-=======
-import Header from "./Component/header/Header";
-const App = () => {
-  return (
-    <div>
-      {/* <Router>
-        <Routes>
-          <Route path="/" element={<MyComponent />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </Router> */}
-      <Header />
-    </div>
->>>>>>> Stashed changes
   );
 };
 
