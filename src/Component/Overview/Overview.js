a35d07fe09d819b3f51943ded9b3a9d759955b37
hoangQuan25 <quan.nh215127@sis.hust.edu.vn>
2024-05-30 11:09:33 +0700

    modify overview page

--- src/Component/Overview/Overview.js
+++ src/Component/Overview/Overview.js
@@ -1,11 +1,23 @@
+import Header from "../header/Header";
 import Sidebar from "../Sidebar";
-import "./Overview.css";
+import NotificationBar from "./notificationBar/NotificationBar";
+import "./Overview.css"; // Import the CSS file for Admin component
+
 const Admin = () => {
   return (
-    <div className="overview-container">
-      <Sidebar />
-      <div className="overview-content">Content go here</div>
+    <div className="admin-container">
+      <div className="side-bar">
+        <Sidebar />
+      </div>
+      <div className="main-content">
+        <Header />
+        <h1 className="overview-heading">Tổng quan</h1>
+        <div className="notification-bar-wrapper">
+          <NotificationBar />
+        </div>
+      </div>
     </div>
   );
 };
+
 export default Admin;
