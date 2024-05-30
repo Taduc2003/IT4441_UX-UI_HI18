9415c1767f3acc4279fd8a550aa8283cf237e287
hoangQuan25 <quan.nh215127@sis.hust.edu.vn>
2024-05-30 09:36:29 +0700

    add header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MyComponent from "./Component/Home/MyComponent";
 import Login from "./Component/Auth/Login";
 import Register from "./Component/Auth/Register";
+<<<<<<< Updated upstream
 import Overview from "./Component/Overview/Overview";
 import Teach from "./Component/Teach/Teach";
 import Schedule from "./Component/Overview/Schedule/Schedule";
@@ -19,6 +20,21 @@ const App = () => {
         <Route path="/teach" element={<Teach />} />
       </Routes>
     </Router>
+=======
+import Header from "./Component/header/Header";
+const App = () => {
+  return (
+    <div>
+      {/* <Router>
+        <Routes>
+          <Route path="/" element={<MyComponent />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router> */}
+      <Header />
+    </div>
+>>>>>>> Stashed changes
   );
 };
 
