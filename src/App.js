42006825037b6d33a1dddbc7419b90265738a90e
chubebanso <vuquanghoanglam@gmail.com>
2024-06-04 18:28:51 +0700

    update home

--- src/App.js
+++ src/App.js
@@ -13,28 +13,44 @@ import KPISetup from "./Component/createKPI/KPISetup";
 import KPISuccess from "./Component/createKPI/KPISuccess";
 import Personal from "./Component/Personal/Personal.js";
 // import Schedule from "./Component/Overview/Schedule/Schedule";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MyComponent />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        {/* <Route path="/schedule" element={<Schedule />} /> */}
-        <Route path="/overview" element={<Overview />} />
-        <Route path="/teach" element={<Teach />} />
-        <Route path="/research" element={<Research />} />
-        {/* Remove the conflicting import statement */}
-        {/* import Service from "./Component/servicekpi/Service"; */}
-        <Route path="/service" element={<Service />} />
-        <Route path="/settings" element={<Setting />} />
-        <Route path="/personal" element={<Personal />} />
-        <Route path="/createkpi" element={<CreateKPI />} />
-        <Route path="/kpisetup" element={<KPISetup />} />
-        <Route path="/kpisuccess" element={<KPISuccess />} />
-      </Routes>
-    </Router>
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <Router>
+        <Routes>
+          <Route path="/" element={<MyComponent />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          {/* <Route path="/schedule" element={<Schedule />} /> */}
+          <Route path="/overview" element={<Overview />} />
+          <Route path="/teach" element={<Teach />} />
+          <Route path="/research" element={<Research />} />
+          {/* Remove the conflicting import statement */}
+          {/* import Service from "./Component/servicekpi/Service"; */}
+          <Route path="/service" element={<Service />} />
+          <Route path="/settings" element={<Setting />} />
+          <Route path="/personal" element={<Personal />} />
+          <Route path="/createkpi" element={<CreateKPI />} />
+          <Route path="/kpisetup" element={<KPISetup />} />
+          <Route path="/kpisuccess" element={<KPISuccess />} />
+        </Routes>
+      </Router>
+    </>
   );
 };
 
