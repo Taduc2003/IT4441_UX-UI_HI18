3afbdfcc6fc2e50231b269350852dc1fc36e1ee5
Taduc2003 <buitaduc139@gmail.com>
2024-06-06 17:43:46 +0700

    modify/responsive-research-and-service

--- src/Component/NghienCuu/Research.js
+++ src/Component/NghienCuu/Research.js
@@ -1,34 +1,39 @@
+import React, { useState } from "react";
 import Sidebar from "../Sidebar";
 import "./Research.css";
 
-import Goals from "./Goals/Goals"
 import ChartComponent from "./ColumnChart/ChartComponet";
 import DetailGoal from "./detailGoal2/DetailGoal";
 import Header from "../header/Header";
 import UploadFileOrImg from "./uploadFile/UploadFileOrImg";
+import GoalBar from "./Goals/GoalBar";
 
 const Admin = () => {
+    const [uploadVisible, setUploadVisible] = useState(false);
+    const toggleUpload = () => {
+      setUploadVisible(!uploadVisible);
+    };
     const data1 = {
-        goal: 'Mục tiêu mức 1',
-        title: 'Số giờ cần nghiên cứu trong kỳ',
-        hocki: 'Kỳ 2023.1',
-        realAverage: 10,
-        needAverage: 96,
-    };
-    const data2 = {
-        goal: 'Mục tiêu mức 2',
-        title: 'Số bài báo nghiên cứu đã công bố trong kỳ',
-        hocki: 'Kỳ  2023.1',
-        realAverage: 1,
-        needAverage: 3,
-    };
-    const data3 = {
-        goal: 'Mục tiêu mức 3',
-        title: 'Số bài báo được đâng trên tạp chí uy tín trong kỳ',
-        hocki: 'Kỳ  2023.1',
-        realAverage: 0,
-        needAverage: 2,
-    };
+      goal: 'Mục tiêu mức 1',
+      title: 'Số giờ cần nghiên cứu trong kỳ',
+      hocki: 'Kỳ 2023.1',
+      realAverage: 10,
+      needAverage: 96,
+  };
+  const data2 = {
+      goal: 'Mục tiêu mức 2',
+      title: 'Số bài báo nghiên cứu đã công bố trong kỳ',
+      hocki: 'Kỳ  2023.1',
+      realAverage: 1,
+      needAverage: 3,
+  };
+  const data3 = {
+      goal: 'Mục tiêu mức 3',
+      title: 'Số bài báo được đâng trên tạp chí uy tín trong kỳ',
+      hocki: 'Kỳ  2023.1',
+      realAverage: 0,
+      needAverage: 2,
+  };
     const researchChart ={
       typee : 'nghiên cứu',
     }
@@ -42,25 +47,19 @@ const Admin = () => {
       status2: 'Đã công bố',
     }
   return (
-    <div className="overview-container">
+    <div className="overview-container-research">
       <Sidebar />
-      <div className ="content-doc">
-      <Header />
-      <div className="titles">Nghiên cứu</div>
-        <div className="ngang">
-        <Goals data ={data1}/> 
-        <ChartComponent data = {researchChart}/>
-        </div>
-        <div className="ngang">
-        <Goals data ={data2}/> 
-        <DetailGoal data = {researchDetail}/>
-        </div>
-        <div className="ngang">
-        <Goals data ={data3}/> 
-        <UploadFileOrImg/>
-        </div>
+      <div className ="main-content-research">
+          <Header />
+          <h1 className="titles-research">Nghiên cứu</h1>
+          <button className="upload-research-button" onClick={toggleUpload}>Tải minh chứng</button>
+          <GoalBar data1={data1} data2={data2} data3={data3} />
+          <ChartComponent data = {researchChart}/>
+          <DetailGoal data = {researchDetail}/>
+        {uploadVisible && <UploadFileOrImg onClose={toggleUpload} />}
       </div>
     </div>
+    
   );
 };
 export default Admin;
--- src/Component/NghienCuu/detailGoal2/DetailGoal.js
+++ src/Component/NghienCuu/detailGoal2/DetailGoal.js
@@ -1,47 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import "./DetailGoal.css";
+
 const DetailGoal = (props) => {
-    
-  return (
-    <div className = "top-contents">
-        <div class="detail-title">Các {props.data.typee} trong kỳ</div>
-        <div class="noidung">
-            {/* <div class="xemtatca"> */}
-                <div class="bills">
-                    <div class="detailss">
-                        <div class="dates">
-                            <div class="semester">{props.data.semester}</div>
+    const [viewAll, setViewAll] = useState(false);
+
+    const handleViewAll = () => {
+        setViewAll(!viewAll);
+    };
+
+    return (
+        <div className="detail-container">
+            <div className="detail-title">Các {props.data.typee} trong kỳ</div>
+            <div className="detail-wrapper">
+                <div className="bills">
+                    <div className="detailss">
+                        <div className="dates">
+                            <div className="semester">{props.data.semester} 
+                            </div>
                         </div>
-                        <div class="subscription-names">
-                            <div class="texts">
-                                <div class="tenbaibao">{props.data.name1}</div>
-                                <div class="ketthuc">Kết thúc: 08/07/2024</div>
+                        <div className="subscription-names">
+                            <div className="texts">
+                                <div className="tenbaibao">{props.data.name1}</div>
+                                <div className="ketthuc">Kết thúc: 08/07/2024</div>
                             </div>
                         </div>
                     </div>
-                        <div class="amounts">
-                            <div class="trangthai">{props.data.status1}</div>
-                        </div>
+                    <div className="amounts">
+                        <div className="trangthai">{props.data.status1}</div>
+                    </div>
                 </div>
-                <div class="bills">
-                    <div class="detailss">
-                        <div class="dates">
-                            <div class="semester">{props.data.semester}</div>
-                        </div>
-                        <div class="subscription-names">
-                            <div class="texts">
-                            <div class="tenbaibao">{props.data.name2}</div>
-                                <div class="ketthucs">Kết thúc: 09/07/2024</div>
+                {viewAll && (
+                    <div className="bills">
+                        <div className="detailss">
+                            <div className="dates">
+                                <div className="semester">{props.data.semester}</div>
+                            </div>
+                            <div className="subscription-names">
+                                <div className="texts">
+                                    <div className="tenbaibao">{props.data.name2}</div>
+                                    <div className="ketthucs">Kết thúc: 09/07/2024</div>
+                                </div>
                             </div>
                         </div>
+                        <div className="amounts">
+                            <div className="trangthai">{props.data.status2}</div>
+                        </div>
                     </div>
-                    <div class="amounts">
-                        <div class="trangthai">{props.data.status2}</div>
-                    </div>
-                </div>
-            {/* </div> */}
+                )}  
+            </div>
+            <button className="view-all-button" onClick={handleViewAll}>
+                    {viewAll ? "Thu gọn" : "Xem tất cả"}
+                </button>  
         </div>
-    </div>
-  );
+    );
 };
+
 export default DetailGoal;
